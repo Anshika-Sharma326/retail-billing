d74e9019c52f4b285f951da5e8d7f86eb8699162
Anshika-Sharma326 <anshikasharma0914@gmail.com>
2026-09-01 16:10:21 +0530

    Connect frontend with deployed backend

--- retail-billing-frontend/src/api/axiosConfig.js
+++ retail-billing-frontend/src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: "https://retail-billing-4.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
